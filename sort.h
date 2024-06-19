6ffe3068dc0ae8dd7689880f6253a6ff2ef8e27a
Younesid12 <idyounes95@gmail.com>
2024-06-18 18:05:23 -0700

    updated

--- sort.h
+++ sort.h
@@ -1,9 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
+#include <stddef.h>
 
 typedef struct listint_s
 {
@@ -11,5 +9,9 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 #endif /*SORT_H*/
